ec4298d520ba0c9b4aa8e06f8dacb07e140f65ec
Saul Ramirez <fernando.ramire@hotmail.com>
2021-08-31 16:39:10 -0600

    ✨ adding memento

---
+++ memento/memento.go
@@ -0,0 +1,40 @@
+package memento
+
+import "fmt"
+
+type State struct {
+	Description string
+}
+
+type memento struct {
+	state State
+}
+
+type originator struct {
+	state State
+}
+
+func (o *originator) NewMemento() memento {
+	return memento{state: o.state}
+}
+
+func (o *originator) ExtractAndStoreState(m memento) {
+	o.state = m.state
+}
+
+type careTaker struct {
+	mementoList []memento
+}
+
+func (c *careTaker) Add(m memento) {
+	c.mementoList = append(c.mementoList, m)
+}
+
+func (c *careTaker) memento(i int) (memento, error) {
+
+	if len(c.mementoList) < i || i < 0 {
+		return memento{}, fmt.Errorf("index not found")
+
+	}
+	return c.mementoList[i], nil
+}
