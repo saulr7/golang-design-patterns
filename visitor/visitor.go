c1e3f8c15289eb45d890c9a0d3b8b378fbc84443
Saul Ramirez <fernando.ramire@hotmail.com>
2021-09-01 18:18:44 -0600

    ✨ adding visitor

---
+++ visitor/visitor.go
@@ -0,0 +1,57 @@
+package visitor
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type MessageA struct {
+	Msg    string
+	Output io.Writer
+}
+
+func (m *MessageA) Accept(v Visitor) {
+	v.VisitA(m)
+}
+
+func (m *MessageA) Print() {
+	if m.Output == nil {
+		m.Output = os.Stdout
+	}
+	fmt.Fprintf(m.Output, "A: %s", m.Msg)
+}
+
+type MessageB struct {
+	Msg    string
+	Output io.Writer
+}
+
+func (m *MessageB) Accept(v Visitor) {
+	v.VisitB(m)
+}
+
+func (m *MessageB) Print() {
+	if m.Output == nil {
+		m.Output = os.Stdout
+	}
+	fmt.Fprintf(m.Output, "B: %s", m.Msg)
+}
+
+type Visitor interface {
+	VisitA(*MessageA)
+	VisitB(*MessageB)
+}
+
+type Visitable interface {
+	Accept(Visitor)
+}
+
+type MessageVisitor struct{}
+
+func (mf *MessageVisitor) VisitA(m *MessageA) {
+	m.Msg = fmt.Sprintf("%s %s", m.Msg, "(Visited A)")
+}
+func (mf *MessageVisitor) VisitB(m *MessageB) {
+	m.Msg = fmt.Sprintf("%s %s", m.Msg, "(Visited B)")
+}
